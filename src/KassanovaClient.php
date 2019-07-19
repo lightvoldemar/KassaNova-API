86b0635e8fcdd5c0a5d5724bac0ca4df41657bf0
Light Voldemar <light.voldemar@gmail.com>
2019-07-19 15:33:48 +0600

    Add new functional

--- src/KassanovaClient.php
+++ src/KassanovaClient.php
@@ -9,14 +9,14 @@ use yii\httpclient\Client;
 class KassanovaClient
 {
     /**
-     * Имя провайдера.
-     */
-    const providerName = 'kassanova';
-
-    /**
      * URL редиректа.
      */
     public $dataRedirectUrl = "ERROR";
+
+    /**
+     * Код платежа.
+     */
+    public $dataOrderSig;
 
     /**
      * URL успешной транзакции.
@@ -31,12 +31,12 @@ class KassanovaClient
     /**
      * Текущая валюта.
      */
-    public $currency;
+    private $currency;
 
     /**
      * Текущий язык.
      */
-    public $language;
+    private $language;
 
     /**
      * API логин.
@@ -52,7 +52,7 @@ class KassanovaClient
      * URL регистрации заказа.
      */
     public $registerUrl = 'https://3ds.kassanova.kz/payment/rest/register.do';
-
+    
     /**
      * Доступные валюты (ISO 4217).
      *
@@ -146,21 +146,12 @@ class KassanovaClient
      * @return boolean
      */
     public function pay($amount,$orderId) {
-        // ID ордера
         $order['order_id'] = $orderId;
         $order['return_url'] = $this->returnUrl;
         $order['fail_url'] = $this->failUrl;
-        $arr = $this->registerOrder($amount,$orderId);
-
-        if(isset($arr['errorCode'])) {
-            return false;
-        } else {
-            // Запись OrderId в БД
-            $this->orderRecord($order['order_num'],$arr['orderId']);
-            // Возврат URL
-            $this->dataRedirectUrl = $arr['formUrl'];
-            return true;
-        }
+        $result = $this->registerOrder($amount,$orderId);
+        $this->dataRedirectUrl = $result['formUrl'];
+        $this->dataOrderSig = $result['orderId'];
     }
 
     /**
@@ -181,6 +172,167 @@ class KassanovaClient
         $data['failUrl'] = $this->failUrl;
 
         return $this->sendRequest($this->registerUrl,$data);
+    }
+
+    
+    private function reverseOrder() {
+        $url_key = "reverse";
+        /*
+         * ?language=ru&
+         * orderId=9231a838-ac68-4a3e-bddb-d9781433d852&
+         * password=password&
+         * userName=userName
+         * */
+
+        // Язык системы
+        $arr[$this->requestParamsArr['LANG']] = $this->params['lang'];
+        // ID заказа
+        $arr[$this->requestParamsArr['ORDER_ID']] = '';
+        // Логин
+        $arr[$this->requestParamsArr['USERNAME']] = $this->params['username'];
+        // Пароль
+        $arr[$this->requestParamsArr['PASSWORD']] = $this->params['password'];
+
+        $this->sendRequest($url_key,$arr);
+    }
+
+    private function refundOrder() {
+        $url_key = "refund";
+
+        /*
+         * amount=500&
+         * currency=643&
+         * language=ru&
+         * orderId=5e97e3fd-1d20-4b4b-a542-f5995f5e8208&
+         * password=password&
+         * userName=userName
+         * */
+
+        // Сумма возврата
+
+        // Валюта
+        //$arr[$this->requestParamsArr['CURRENCY']] = '';
+        // Язык системы
+        $arr[$this->requestParamsArr['LANG']] = $this->params['lang'];
+        // ID заказа
+        $arr[$this->requestParamsArr['ORDER_ID']] = '';
+        // Логин
+        $arr[$this->requestParamsArr['USERNAME']] = $this->params['username'];
+        // Пароль
+        $arr[$this->requestParamsArr['PASSWORD']] = $this->params['password'];
+
+        // Отправка запроса
+        $this->sendRequest($url_key,$arr);
+    }
+
+    private function getOrderStatus() {
+        $url_key = "getOrderStatus";
+
+        /*
+         * orderId=b8d70aa7-bfb3-4f94-b7bb-aec7273e1fce&
+         * language=ru&
+         * password=password&
+         * userName=userName
+         * */
+
+        // Язык системы
+        $arr[$this->requestParamsArr['LANG']] = $this->params['lang'];
+        // ID заказа
+        $arr[$this->requestParamsArr['ORDER_ID']] = '';
+        // Логин
+        $arr[$this->requestParamsArr['USERNAME']] = $this->params['username'];
+        // Пароль
+        $arr[$this->requestParamsArr['PASSWORD']] = $this->params['password'];
+
+        // Отправка запроса
+        $this->sendRequest($url_key,$arr);
+    }
+
+    private function getOrderStatusExtended() {
+        $url_key = "getOrderStatusExtended";
+
+        /*
+         * userName=userName&
+         * password=password&
+         * orderId=b9054496-c65a-4975-9418-1051d101f1b9&
+         * language=ru&
+         * merchantOrderNumber=0784sse49d0s134567890
+         * */
+
+        // Логин
+        $arr[$this->requestParamsArr['USERNAME']] = $this->params['username'];
+        // Пароль
+        $arr[$this->requestParamsArr['PASSWORD']] = $this->params['password'];
+        // ID заказа
+        $arr[$this->requestParamsArr['ORDER_ID']] = '';
+        // Язык системы
+        $arr[$this->requestParamsArr['LANG']] = $this->params['lang'];
+        // ID ордера по мерчанту
+        $arr[$this->requestParamsArr['MERCHANT_ORDER_NUM']] = '';
+
+        // Отправка запроса
+        $this->sendRequest($url_key,$arr);
+    }
+
+    private function getLastOrdersForMerchants() {
+        $url_key = "getLastOrdersForMerchants";
+
+        /*
+         * userName=userName&
+         * password=password&
+         * language=ru&
+         * page=0&
+         * size=100&
+         * from=20141009160000&
+         * to=20141111000000&
+         * transactionStates=DEPOSITED,REVERSED&
+         * merchants=SevenEightNine&
+         * searchByCreatedDate=false
+         * */
+
+        // Логин
+        $arr[$this->requestParamsArr['USERNAME']] = $this->params['username'];
+        // Пароль
+        $arr[$this->requestParamsArr['PASSWORD']] = $this->params['password'];
+        // Язык системы
+        $arr[$this->requestParamsArr['LANG']] = $this->params['lang'];
+        // Номер страницы
+        $arr[$this->requestParamsArr['PAGE']] = '';
+        // Кол-во записей на  странице
+        $arr[$this->requestParamsArr['SIZE']] = '';
+        // Дата начала
+        $arr[$this->requestParamsArr['FROM']] = '';
+        // Дата окончания
+        $arr[$this->requestParamsArr['TO']] = '';
+        // Статусы заказов
+        $arr[$this->requestParamsArr['transactionStates']] = '';
+        // Список мерчантов
+        //$arr[$this->requestParamsArr['merchants']] = '';
+        // Использовать дату оплаты или дату создания заказов
+        $arr[$this->requestParamsArr['searchByCreatedDate']] = 'false';
+
+        // Отправка запроса
+        $this->sendRequest($url_key,$arr);
+    }
+
+    private function verifyEnrollment() {
+        $url_key = "verifyEnrollment";
+
+        /*
+         * userName=userName&
+         * password=password&
+         * pan=4111111111111111
+         * */
+
+        // Логин
+        $arr[$this->requestParamsArr['USERNAME']] = $this->params['username'];
+        // Пароль
+        $arr[$this->requestParamsArr['PASSWORD']] = $this->params['password'];
+        // Номер карты
+        $arr[$this->requestParamsArr['PAN']] = '';
+
+        // Отправка запроса
+        $this->sendRequest($url_key,$arr);
     }
 
     /**
